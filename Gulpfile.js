6b57a5d0c967ceee08d9547593e9ea3b15236f39
Jimmy Zooler <jimmy.zooler.com>
2016-12-07 21:36:05 -0500

    Added livereload, image compression, and stylesheet compression.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,15 +5,78 @@
  */
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
     
+//Styles
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(notify({ message: 'Styles task complete' }));
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+// Scripts
+//gulp.task('scripts', function() {
+//  return gulp.src('js/**/*.js')
+//    .pipe(jshint('.jshintrc'))
+//    .pipe(jshint.reporter('default'))
+//    .pipe(concat('main.js'))
+//    .pipe(gulp.dest('dist/js'))
+//    .pipe(rename({suffix: '.min'}))
+//    .pipe(uglify())
+//    .pipe(gulp.dest('dist/js'))
+//    .pipe(notify({ message: 'Scripts task complete' }));
+//});
+
+//Compress Images
+gulp.task('images', function() {
+  return gulp.src('images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function() {
+  return del(['dist/styles', 'dist/images']);
+});
+
+// Default Task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('sass/**/*.scss', ['styles']);
+
+  // Watch .js files
+  //gulp.watch('js/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('images/**/*', ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
 });
