c73ebecd197a2ea3221a775ca04fe34b5521af81
Jimmy Zooler <jimmy.zooler.com>
2016-12-07 19:50:05 -0500

    Added gitignore, gulpfile, package.json, and set up gulp to compile sass.

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+    
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
