d472bd7aeae4dde9afd639081a430e7a67cb5ad5
Jimmy Zooler <jimmy.zooler.com>
2016-12-09 17:48:00 -0500

    Subscription bar finished, styled, connected to custom fields, and responsive.

--- template-home.php
+++ template-home.php
@@ -14,11 +14,12 @@ Template Name: Home Page
                 <h3>Conversations that matter with incredible human beings...</h3>
         </div>
     </section>
-    <section class="second-section">
+    <section class="subscription-bar" style="background-image: url('<?php echo get_template_directory_uri(); ?>/dist/images/subscription-background.jpg');">
         <div class="row">
-            
+            <p><?php the_field('subscription_heading'); ?></p>
+            <button><?php the_field('subscription_button_text'); ?></button>
         </div>
-
+        <div class='overlay-blue'></div>
     </section>
 
 <?php get_footer(); ?>
