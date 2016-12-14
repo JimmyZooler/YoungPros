02da3f85f9edcd567bdf56291c03d9521eff11b6
Jimmy Zooler <jimmy.zooler.com>
2016-12-14 17:34:49 -0500

    Fixed post meta for blog page, add bullet and avatar.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,16 +25,16 @@ function youngpros_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'youngpros' ),
+		esc_html_x( '%s', 'post date', 'youngpros' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'youngpros' ),
+		esc_html_x( 'By %s', 'post author', 'youngpros' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo get_avatar( get_the_author_meta('user_email'), $size = '40') . '<span class="byline"> ' . $byline . '&nbsp;&nbsp;&bull;&nbsp;&nbsp;</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
