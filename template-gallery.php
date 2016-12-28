4ba0d5dc568bdba25b8331ddf640676dfc17ae07
Jimmy Zooler <jimmy.zooler.com>
2016-12-28 15:16:28 -0500

    Added and styled header to episodes page.

--- template-gallery.php
+++ template-gallery.php
@@ -5,7 +5,12 @@ Template Name: Gallery Page
 ?>
 
 <?php get_header(); ?>
-		
+<section id="episode-page-header" style="background-image: url('<?php echo get_template_directory_uri(); ?>/dist/images/subscription-background.jpg');">
+        <div class="row">
+            <h2><?php the_field('page_header'); ?></h2>
+        </div>
+        <div class='overlay-blue'></div>
+</section>
 <div class="container">
     <div class="row">
         <?php get_template_part( 'template-parts/gallery'); ?>
