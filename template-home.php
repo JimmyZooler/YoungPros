b0a95c0eca6dbff1fbd03e70b11933c1aea0bc74
Jimmy Zooler <jimmy.zooler.com>
2016-12-29 13:40:41 -0500

    Added homepage hero image.

--- template-home.php
+++ template-home.php
@@ -6,13 +6,12 @@ Template Name: Home Page
 
 <?php get_header(); ?>
 			
-    <section id="home-page-intro">
-        <div class="row text-center">
+    <section id="home-page-intro" style="background-image:url('<?php echo get_template_directory_uri(); ?>/dist/images/hero.png')">
+<!--        <div class="row text-center">
                 <h2>Welcome To</h2>
                 <h1>The Young Pros Show</h1>
-                <div id="video-container"></div>
                 <h3>Conversations that matter with incredible human beings...</h3>
-        </div>
+        </div>-->
     </section>
     <section class="subscription-bar" style="background-image: url('<?php echo get_template_directory_uri(); ?>/dist/images/subscription-background.jpg');">
         <div class="row">
