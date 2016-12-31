ef558e5b132d029c41288ba2f6b2973c344444fe
Jimmy Zooler <jimmy.zooler.com>
2016-12-30 22:55:52 -0500

    Removed unused social icons.

--- template-parts/social-icon-bar.php
+++ template-parts/social-icon-bar.php
@@ -7,9 +7,8 @@ Template to post Social Icons in a row
 <div class="row">
     <ul class="social-icons">
         <li class="social-icon"><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-        <li class="social-icon"><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+        <li class="social-icon"><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
         <li class="social-icon"><a href=""><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-        <li class="social-icon"><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
     </ul>
 <!--    <ul class="social-icons">
                                             <li class="social-menu-icon"><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
