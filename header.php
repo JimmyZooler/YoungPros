ec7efd216aeacfb68b6843367250e387fe809d51
Jimmy Zooler <jimmy.zooler.com>
2017-01-22 15:58:57 -0500

    Final edits to homepage signups, social links.

--- header.php
+++ header.php
@@ -38,8 +38,8 @@
                                 <div class="top-bar-section">
                                     <div class="social-menu right">
                                         <ul class="social-menu-icons">
-                                            <li class="social-menu-icon"><a href="https://www.facebook.com/youngprosmvmt/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                            <li class="social-menu-icon"><a href="https://www.instagram.com/youngprosmvmt/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                            <li class="social-menu-icon"><a href="https://www.facebook.com/youngprosshow/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                            <li class="social-menu-icon"><a href="https://www.instagram.com/youngprosshow/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                                             <li class="social-menu-icon"><a href="https://www.youtube.com/channel/UChwRZLioUjxODgHFOyP-7uA" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a></li></ul>
                                     </div>
                                     <div class="right">
--- template-home.php
+++ template-home.php
@@ -10,8 +10,8 @@ Template Name: Home Page
         <div class='webinar-helper'>    
             <div class="webinar text-center">
                 <h1>The Young Pros Show</h1>
-                <h2>Launch your <span>2017</span><br><span id="game-plan">game plan</span></h2>
-                <p>Step by Step Webinar into having<br>your Best Year Ever!</p>
+                <h2>Get Your <span>12</span> month<br><span id="game-plan">game plan</span></h2>
+                <p>Free Step-By-Step Webinar to turn<br>Your Vision into Reality.</p>
                 <div class="form-inner">
                     <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
                     <style type="text/css">
@@ -38,7 +38,7 @@ Template Name: Home Page
                                     <div class="response" id="mce-success-response" style="display:none"></div>
                             </div>     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_aafca6008e577c3f07f4452bc_f88e40bfc2" tabindex="-1" value=""></div>
-                        <input type="submit" value="I Can't Wait!" name="subscribe" id="mc-embedded-subscribe" class="button">
+                        <input type="submit" value="Let's Do It!" name="subscribe" id="mc-embedded-subscribe" class="button">
                         </div>
                     </form>
                     </div>
@@ -52,24 +52,12 @@ Template Name: Home Page
             <p><?php the_field('subscription_heading'); ?></p>
             
             <a href="#subscribe-popup" class="open-subscribe-popup">
-                <!--<div id="orange-arrow" style="background-image:url('<?php echo get_template_directory_uri(); ?>/dist/images/arrow_orange.png');" ></div>-->
+                <div id="orange-arrow" style="background-image:url('<?php echo get_template_directory_uri(); ?>/dist/images/arrow_orange.png');" ></div>
                 <button class="subscription-button"><?php the_field('subscription_button_text'); ?></button>
             </a>
         </div>
         <div class='overlay-blue'></div>
     </section>
-<section id="episodes">
-    <div class="row text-center">
-        <h2>Recent Episodes</h2>
-        <?php get_template_part( 'template-parts/recent-gallery', 'none' ); ?>
-    </div>
-    <div class="view-all">
-        <a href="<?php echo esc_url( home_url( '/episodes' ) ); ?>" ><button class="view-all-button">View All</button></a>
-    </div>
-</section>
-<div class="row">
-    <?php get_template_part( 'template-parts/subscription-popup', 'none' ); ?>
-</div>
 <?php get_template_part( 'template-parts/subscription-bar', 'none' ); ?>
 
 <?php get_footer(); ?>
