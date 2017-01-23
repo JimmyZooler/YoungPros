9a05ff93c7331a2ee4975795d26dc7cbda630cd9
Jimmy Zooler <jimmy.zooler.com>
2017-01-22 22:25:43 -0500

    Added session storage to js so popup only shows once per session.

--- js/little-magnific.js
+++ js/little-magnific.js
@@ -3,10 +3,16 @@
  */
 
 jQuery(document).ready(function(){
-    
+    if(sessionStorage["PopupShown"] != 'yes'){ 
+         showPopup();
+     };
+});
+
+function showPopup() {
     setTimeout(function(){
    
         jQuery.magnificPopup.open({
             items: {src: '#subscribe-popup'},type: 'inline'}, 0);
     }, 5000);
-});
+    sessionStorage["PopupShown"] = 'yes';
+};
